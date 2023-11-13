286c42efb341166caba0a0ca3fb6479abbb9f652
SpikeHD <spikegdofficial@gmail.com>
2023-11-12 17:36:15 -0800

    feat(macos): working macos pids

--- src/apple.rs
+++ src/apple.rs
@@ -1,9 +1,38 @@
 use std::{error::Error, fmt, process::Command};
 
-use crate::{ConnectionTrait, Result};
+use crate::{ConnectionTrait, Result, Window};
 
-const PREFIX: &str = r#"tell application "System Events""#;
-const SUFFIX: &str = r#"to get the title of every window of every process"#;
+const SCRIPT: &str = r#"
+set infoList to {}
+
+-- List of processes
+tell application "System Events"
+    set procList to processes
+end tell
+
+-- Iterate processes
+repeat with proc in procList
+    try
+        -- Windows
+        set winList to windows of proc
+        set winPid to id of proc
+
+        -- Iterate windows
+        repeat with win in winList
+            set winName to name of win
+
+            -- Add to list
+            set end of infoList to "{" & winPid & "," & quoted form of winName & "}"
+        end repeat
+    on error errMsg
+        -- do nothing
+        log errMsg
+    end try
+end repeat
+
+-- Output
+copy infoList as string to stdout
+"#;
 const PERMISSION_ERROR: &str = "osascript is not allowed assistive access";
 
 pub struct Connection;
@@ -11,14 +40,15 @@ impl ConnectionTrait for Connection {
     fn new() -> Result<Self> {
         Ok(Self)
     }
-    fn window_titles(&self) -> Result<Vec<String>> {
-        let arguments = &["-ss", "-e", &format!("{} {}", PREFIX, SUFFIX)];
+    fn window_titles(&self) -> Result<Vec<Window>> {
+        let arguments = &["-ss", "-e", &format!("{}", SCRIPT)];
         let command = Command::new("osascript")
             .args(arguments)
             .output()
             .expect("failed to execute AppleScript command");
 
         let error = String::from_utf8_lossy(&command.stderr);
+
         match error.contains(PERMISSION_ERROR) {
             true => Err(WindowTitleError::NoAccessibilityPermission.into()),
             false => Ok(split(&String::from_utf8_lossy(&command.stdout))),
@@ -42,23 +72,16 @@ impl fmt::Display for WindowTitleError {
 }
 impl Error for WindowTitleError {}
 
-fn split(mut string: &str) -> Vec<String> {
-    let mut titles = Vec::new();
-    while let Some(start) = string.find('"') {
-        let end = string[start + 1..].find('"').unwrap();
-        titles.push(string[start + 1..][..end].to_string());
-        string = &string[start + 1..][end + 1..];
-    }
-    titles
-}
+fn split(output: &str) -> Vec<Window> {
+    let mut windows = Vec::new();
+    
+    // Output is in pattern: `"\{pid,'name'}{pid,'name'}"`
+    for window in output.replace("\"", "").split("{") {
+      let pid = window.split(",").next().unwrap().parse::<u32>().unwrap_or(0);
+      let title = window.split("'").nth(1).unwrap_or("").to_string();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_split() {
-        let string = r#"{{}, {"0"}, {"1", "2"}}"#;
-        assert_eq!(split(string), &["0", "1", "2"]);
+      windows.push(Window { title, pid });
     }
+
+    windows
 }
