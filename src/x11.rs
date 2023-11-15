1d1dde9e0f19fd747f23667db5a9eaa920442279
SpikeHD <spikegdofficial@gmail.com>
2023-11-15 10:25:10 -0800

    fix(linux): xcb dep issues

--- src/x11.rs
+++ src/x11.rs
@@ -1,7 +1,4 @@
-use xcb::{
-    xproto::{get_property, intern_atom, Atom, ATOM_WINDOW},
-    Connection as XConnection, ATOM_INTEGER, ATOM_CARDINAL
-};
+use xcb::{Connection as XConnection, x::{self, Atom}};
 
 use crate::{ConnectionTrait, Result, Window};
 
@@ -16,18 +13,30 @@ pub struct Connection {
 impl ConnectionTrait for Connection {
     fn new() -> Result<Self> {
         let connection = XConnection::connect(None)?.0;
-        let client_list = intern_atom(&connection, false, "_NET_CLIENT_LIST")
-            .get_reply()?
-            .atom();
-        let string = intern_atom(&connection, false, "UTF8_STRING")
-            .get_reply()?
-            .atom();
-        let window_name = intern_atom(&connection, false, "_NET_WM_NAME")
-            .get_reply()?
-            .atom();
-        let pid = intern_atom(&connection, false, "_NET_WM_PID")
-            .get_reply()?
-            .atom();
+        let client_list = connection.send_request(&x::InternAtom {
+            only_if_exists: false,
+            name: "_NET_CLIENT_LIST".as_bytes(),
+        });
+        let client_list = connection.wait_for_reply(client_list)?.atom();
+    
+        let string = connection.send_request(&x::InternAtom {
+            only_if_exists: false,
+            name: "UTF8_STRING".as_bytes(),
+        });
+        let string = connection.wait_for_reply(string)?.atom();
+    
+        let window_name = connection.send_request(&x::InternAtom {
+            only_if_exists: false,
+            name: "_NET_WM_NAME".as_bytes(),
+        });
+        let window_name = connection.wait_for_reply(window_name)?.atom();
+
+        let pid = connection.send_request(&x::InternAtom {
+            only_if_exists: false,
+            name: "_NET_WM_PID".as_bytes(),
+        });
+        let pid = connection.wait_for_reply(pid)?.atom();
+
         Ok(Self {
             connection,
             client_list,
@@ -44,46 +53,44 @@ impl ConnectionTrait for Connection {
             .map(|screen| screen.root())
             .map(|window| {
                 (
-                    get_property(
-                        &self.connection,
-                        false,
+                    self.connection.send_request(&x::GetProperty {
+                        delete: false,
                         window,
-                        self.client_list,
-                        ATOM_WINDOW,
-                        0,
-                        1024,
-                    ),
-                    get_property(
-                        &self.connection,
-                        false,
+                        property: self.client_list,
+                        r#type: x::ATOM_NONE,
+                        long_offset: 0,
+                        long_length: 1024,
+                    }),
+                    self.connection.send_request(&x::GetProperty {
+                        delete: false,
                         window,
-                        self.pid,
-                        ATOM_CARDINAL,
-                        0,
-                        4
-                    )
+                        property: self.pid,
+                        r#type: x::ATOM_CARDINAL,
+                        long_offset: 0,
+                        long_length: 4
+                    })
                 )
             })
             .filter_map(|cookie| Some((
-                cookie.0.get_reply().ok(),
-                cookie.1.get_reply().ok()
+                self.connection.wait_for_reply(cookie.0).ok(),
+                self.connection.wait_for_reply(cookie.1).ok()
             )));
 
         let windows = pairs.filter_map(|pair| {
             let window = pair.0?.value()[0];
             let pid = pair.1?;
-            let pid = if pid.value_len() <= 0 { 0 } else { pid.value()[0] };
+            let pid = if pid.value::<u32>().len() <= 0 { 0 } else { pid.value()[0] };
 
-            let name = get_property(
-                &self.connection,
-                false,
+            let name = self.connection.send_request(&x::GetProperty {
+                delete: false,
                 window,
-                self.window_name,
-                self.string,
-                0,
-                1024,
-            ).get_reply().ok()?.value().to_vec();
+                property: self.window_name,
+                r#type: self.string,
+                long_offset: 0,
+                long_length: 1024,
+            });
 
+            let name: Vec<u8> = self.connection.wait_for_reply(name).ok()?.value().to_vec();
             let name = String::from_utf8(name).ok()?;
             let name = name.trim_matches('\0').to_string();
 
